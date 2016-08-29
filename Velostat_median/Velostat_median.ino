491bac92dfdf20bd3a53b4b39486d93e411a79cb
crazymach <benhyee@gmail.com>
2016-08-28 18:00:58 -0700

    Velostat median add

---
+++ Velostat_median/Velostat_median.ino
@@ -0,0 +1,48 @@
+#include<math.h>
+#define NUM_READS 50;
+int velo = 1;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  double sensorValue = readSensor(velo);
+  Serial.println(velo);
+  delay(50);
+}
+double readSensor(int sensorpin){
+   // read multiple values and sort them to take the mode
+   int sortedValues[NUM_READS];
+   for(int i=0;i<NUM_READS;i++){
+     int value = analogRead(sensorpin);
+     int j;
+     if(value<sortedValues[0] || i==0){
+        j=0; //insert at first position
+     }
+     else{
+       for(j=1;j<i;j++){
+          if(sortedValues[j-1]<=value && sortedValues[j]>=value){
+            // j is insert position
+            break;
+          }
+       }
+     }
+     for(int k=i;k>j;k--){
+       // move all values higher than current reading up one position
+       sortedValues[k]=sortedValues[k-1];
+     }
+     sortedValues[j]=value; //insert current reading
+   }
+   //return scaled mode of 10 values
+   float returnval = 0;
+   for(int i=NUM_READS/2-5;i<(NUM_READS/2+5);i++){
+     returnval +=sortedValues[i];
+   }
+   return returnval * (5.0/1023.0);  //returns voltage values)
+}
+
